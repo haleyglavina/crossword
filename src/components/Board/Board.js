68bdfbe34d94c8f8800ed24f7c2c58b90c0b5568
Haley Glavina <haleyglav@gmail.com>
2020-08-21 00:23:40 -0400

    Board identifies when all words are filled in correctly, and which are not filled in correctly! Console logs correctness for each word

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BoardRow from '../BoardRow/BoardRow';
+import {v4 as uuid} from 'uuid';
 import './Board.scss';
 const col = 18;
 
@@ -36,7 +37,118 @@ const inputCells = {
   r5c10: 1, //O
   r5c11: 1, //N
 
+  r5c6: 1, //'s'
+  r6c6: 1, //'u'
+  r7c6: 1, //'p'
+  r8c6: 1, //'p'
+  r9c6: 1, //'o'
+  r10c6: 1, //'r'
+  r11c6: 1, //'t'
+  r12c6: 1, //'i'
+  r13c6: 1, //'v'
+  r14c6: 1, //'e'
+
+  r5c16: 1, //'q'
+  r6c16: 1, //'u'
+  r7c16: 1, //'i'
+  r8c16: 1, //'c'
+  r9c16: 1, //'k'
+
+  r6c14: 1, //'s'
+  r7c14: 1, //'p'
+  r8c14: 1, //'a'
   
+  r7c2: 1, //'g'
+  r7c3: 1, //'r'
+  r7c4: 1, //'u'
+  r7c5: 1, //'m'
+  r7c6: 1, //'p'
+  r7c7: 1, //'y'
+
+  r7c10: 1, //'e'
+  r8c10: 1, //'v'
+  r9c10: 1, //'i'
+  r10c10: 1, //'l'
+  r11c10: 1, //'d'
+  r12c10: 1, //'a'
+  r13c10: 1, //'n'
+  r14c10: 1, //'i'
+  r15c10: 1, //'i'
+  r16c10: 1, //'l'
+
+  r7c12: 1, //'s'
+  r7c13: 1, //'i'
+  r7c14: 1, //'p'
+  r7c15: 1, //'s'
+  r7c16: 1, //'i'
+  r7c17: 1, //'p'
+
+  r7c12: 1, //'s'
+  r8c12: 1, //'m'
+  r9c12: 1, //'a'
+  r10c12: 1, //'l'
+  r11c12: 1, //'l'
+  r12c12: 1, //'w'
+  r13c12: 1, //'i'
+  r14c12: 1, //'n'
+  r15c12: 1, //'s'
+
+  r8c8: 1, //'b'
+  r9c8: 1, //'a'
+  r10c8: 1, //'c'
+  r11c8: 1, //'k'
+  r12c8: 1, //'g'
+  r13c8: 1, //'a'
+  r14c8: 1, //'m'
+  r15c8: 1, //'m'
+  r16c8: 1, //'o'
+  r17c8: 1, //'n'
+
+  r9c3: 1, //'a'
+  r9c4: 1, //'u'
+  r9c5: 1, //'t'
+  r9c6: 1, //'o'
+  r9c7: 1, //'m'
+  r9c8: 1, //'a'
+  r9c9: 1, //'g'
+  r9c10: 1, //'i'
+  r9c11: 1, //'c'
+  r9c12: 1, //'a'
+  r9c13: 1, //'l'
+
+  r10c1: 1, //'c'
+  r11c1: 1, //'a'
+  r12c1: 1, //'t'
+
+  r11c0: 1, //'h'
+  r11c1: 1, //'a'
+  r11c2: 1, //'r'
+  r11c3: 1, //'d'
+  r11c4: 1, //'e'
+  r11c5: 1, //'s'
+  r11c6: 1, //'t'
+
+  r12c12: 1, //'w'
+  r12c13: 1, //'i'
+  r12c14: 1, //'l'
+  r12c15: 1, //'d'
+
+  r13c4: 1, //'t'
+  r14c4: 1, //'i'
+  r15c4: 1, //'m'
+  r16c4: 1, //'e'
+
+  r14c0: 1, //'o'
+  r14c1: 1, //'f'
+  r14c2: 1, //'f'
+  r14c3: 1, //'l'
+  r14c4: 1, //'i'
+  r14c5: 1, //'n'
+  r14c6: 1, //'e'
+
+  r17c6: 1, //'p'
+  r17c7: 1, //'u'
+  r17c8: 1, //'n'
 }
 
 // Create list of board rows
@@ -47,6 +159,7 @@ for (let i=0; i<col; i++) {
       row={i} 
       col={col} 
       inputCells={inputCells}
+      key={uuid()}
     />
   )
 }
@@ -111,7 +224,7 @@ class Board extends React.Component {
     else {
       console.log("4 across is wrong...");
     }
-/*
+
     if (e.target.r5c6.value.toLowerCase() === 's'
       && e.target.r6c6.value.toLowerCase() === 'u'
       && e.target.r7c6.value.toLowerCase() === 'p'
@@ -177,17 +290,122 @@ class Board extends React.Component {
       && e.target.r7c15.value.toLowerCase() === 's'
       && e.target.r7c16.value.toLowerCase() === 'i'
       && e.target.r7c17.value.toLowerCase() === 'p')
-        console.log("SIPSIP is correct!");
+        console.log("SIP SIP is correct!");
     else {
       console.log("9 across is wrong...");
     }
-*/
+
+    if (e.target.r7c12.value.toLowerCase() === 's'
+      && e.target.r8c12.value.toLowerCase() === 'm'
+      && e.target.r9c12.value.toLowerCase() === 'a'
+      && e.target.r10c12.value.toLowerCase() === 'l'
+      && e.target.r11c12.value.toLowerCase() === 'l'
+      && e.target.r12c12.value.toLowerCase() === 'w'
+      && e.target.r13c12.value.toLowerCase() === 'i'
+      && e.target.r14c12.value.toLowerCase() === 'n'
+      && e.target.r15c12.value.toLowerCase() === 's')
+        console.log("SMALL WINS is correct!");
+    else {
+      console.log("9 down is wrong...");
+    }
+
+    if (e.target.r8c8.value.toLowerCase() === 'b'
+      && e.target.r9c8.value.toLowerCase() === 'a'
+      && e.target.r10c8.value.toLowerCase() === 'c'
+      && e.target.r11c8.value.toLowerCase() === 'k'
+      && e.target.r12c8.value.toLowerCase() === 'g'
+      && e.target.r13c8.value.toLowerCase() === 'a'
+      && e.target.r14c8.value.toLowerCase() === 'm'
+      && e.target.r15c8.value.toLowerCase() === 'm'
+      && e.target.r16c8.value.toLowerCase() === 'o'
+      && e.target.r17c8.value.toLowerCase() === 'n')
+        console.log("BACKGAMMON is correct!");
+    else {
+      console.log("10 down is wrong...");
+    }
+
+    if (e.target.r9c3.value.toLowerCase() === 'a'
+      && e.target.r9c4.value.toLowerCase() === 'u'
+      && e.target.r9c5.value.toLowerCase() === 't'
+      && e.target.r9c6.value.toLowerCase() === 'o'
+      && e.target.r9c7.value.toLowerCase() === 'm'
+      && e.target.r9c8.value.toLowerCase() === 'a'
+      && e.target.r9c9.value.toLowerCase() === 'g'
+      && e.target.r9c10.value.toLowerCase() === 'i'
+      && e.target.r9c11.value.toLowerCase() === 'c'
+      && e.target.r9c12.value.toLowerCase() === 'a'
+      && e.target.r9c13.value.toLowerCase() === 'l')
+        console.log("AUTOMAGICAL is correct!");
+    else {
+      console.log("11 across is wrong...");
+    }  
+    
+    if (e.target.r10c1.value.toLowerCase() === 'c'
+      && e.target.r11c1.value.toLowerCase() === 'a'
+      && e.target.r12c1.value.toLowerCase() === 't')
+        console.log("CAT is correct!");
+    else {
+      console.log("12 down is wrong...");
+    }
+
+    if (e.target.r11c0.value.toLowerCase() === 'h'
+      && e.target.r11c1.value.toLowerCase() === 'a'
+      && e.target.r11c2.value.toLowerCase() === 'r'
+      && e.target.r11c3.value.toLowerCase() === 'd'
+      && e.target.r11c4.value.toLowerCase() === 'e'
+      && e.target.r11c5.value.toLowerCase() === 's'
+      && e.target.r11c6.value.toLowerCase() === 't')
+        console.log("HARDEST is correct!");
+    else {
+      console.log("13 across is wrong...");
+    }
+
+    if (e.target.r12c12.value.toLowerCase() === 'w'
+      && e.target.r12c13.value.toLowerCase() === 'i'
+      && e.target.r12c14.value.toLowerCase() === 'l'
+      && e.target.r12c15.value.toLowerCase() === 'd')
+        console.log("WILD is correct!");
+    else {
+      console.log("14 across is wrong...");
+    }
+
+    if (e.target.r13c4.value.toLowerCase() === 't'
+      && e.target.r14c4.value.toLowerCase() === 'i'
+      && e.target.r15c4.value.toLowerCase() === 'm'
+      && e.target.r16c4.value.toLowerCase() === 'e')
+        console.log("TIME is correct!");
+    else {
+      console.log("15 down is wrong...");
+    }
+
+    if (e.target.r14c0.value.toLowerCase() === 'o'
+      && e.target.r14c1.value.toLowerCase() === 'f'
+      && e.target.r14c2.value.toLowerCase() === 'f'
+      && e.target.r14c3.value.toLowerCase() === 'l'
+      && e.target.r14c4.value.toLowerCase() === 'i'
+      && e.target.r14c5.value.toLowerCase() === 'n'
+      && e.target.r14c6.value.toLowerCase() === 'e')
+        console.log("OFFLINE is correct!");
+    else {
+      console.log("14 across is wrong...");
+    }
+
+    if (e.target.r17c6.value.toLowerCase() === 'p'
+      && e.target.r17c7.value.toLowerCase() === 'u'
+      && e.target.r17c8.value.toLowerCase() === 'n')
+        console.log("PUN is correct!");
+    else {
+      console.log("17 across is wrong...");
+    }
+
     return;
   }
 
+
+
   render() {
     return (
-      <form onSubmit={this.submitHandler}>
+      <form onSubmit={this.submitHandler} autoComplete="off">
         {rowsInBoard}
         <button type="submit">Submit all</button>
       </form>
